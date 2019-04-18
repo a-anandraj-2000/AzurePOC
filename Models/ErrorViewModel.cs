bbcb4aaea0861c2fc631a7a6d48f39f465dbaeef
a-anandraj-2000 <a_anandraj_2000@yahoo.com>
2019-04-18 15:33:42 -0500

    Added files from my local computer

---
+++ Models/ErrorViewModel.cs
@@ -0,0 +1,11 @@
+using System;
+
+namespace AzurePOC.Models
+{
+    public class ErrorViewModel
+    {
+        public string RequestId { get; set; }
+
+        public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
+    }
+}
